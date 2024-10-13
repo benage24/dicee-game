1a1985f5aafc547a933e3b039175015acf113623
andy benage <andy.benage@africauniondev.com>
2024-10-13 17:41:16 +0100

    add changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,90 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    backgroundColor: Colors.red,
+    backgroundColor: const Color(0xFF1877F2),
     appBar: AppBar(
-      title: const Text("Dice Game"),
-      backgroundColor: Colors.red,
+      title: const Text("Dice Game",style: TextStyle(color:Colors.white),),
+      backgroundColor: const Color(0xFF1877F2),
     ),
     body: const DiceePage(),
   )));
 }
 
-class DiceePage extends StatelessWidget {
+class DiceePage extends StatefulWidget {
   const DiceePage({super.key});
- 
+
+  @override
+  State<DiceePage> createState() => _DiceePageState();
+}
+
+class _DiceePageState extends State<DiceePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int score = 0;
+  void getDiceeNumber() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+      if (rightDiceNumber == leftDiceNumber) {
+        score += 1;
+        print(score);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
- var leftDiceNumber = 5;
-  var rightDiceNumber = 2;
     return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Column(
+     mainAxisAlignment: MainAxisAlignment.center,
+
         children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () {
-                  print('kj');
-                },
-                child:  Image(
-                  image: AssetImage('images/dice$leftDiceNumber.png'),
-                ),
+            // Text widget at the top
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0), // Adds space at the top
+              child: Text(
+                'Score: $score',
+                style: const TextStyle(fontSize: 44, color: Colors.white),
               ),
             ),
+          const SizedBox(height: 50), 
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {
+                      getDiceeNumber();
+                    },
+                    child: Image(
+                      image: AssetImage('images/dice$leftDiceNumber.png'),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {
+                      getDiceeNumber();
+                    },
+                    child: Image(
+                      image: AssetImage('images/dice$rightDiceNumber.png'),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () {
-                  print('kjnnn');
-                },
-                child:  Image(
-                 image:  AssetImage('images/dice$rightDiceNumber.png'),
-                ),
-              ),
-            ),
-          )
         ],
       ),
     );
